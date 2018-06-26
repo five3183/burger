b85927be12a1096d29c7ea0e9d2dc8330f6df852
five3183 <five3183@gmail.com>
2018-06-26 13:52:39 -0400

    orm fix

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 var orm = {
     all: function(tableInput, cb) {
         connection.query("SELECT * FROM " + tableInput + ";", function(err, result) {
